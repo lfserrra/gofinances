c282a00638481a71a540f906f0c8a3e1d13f5a11
Luis Fernando Serra <lfserrra@gmail.com>
2022-03-25 16:08:22 -0300

    Adicionado mock auth google

--- jest.config.js
+++ jest.config.js
@@ -10,5 +10,6 @@ module.exports = {
   setupFilesAfterEnv: [
     "@testing-library/jest-native/extend-expect",
     "jest-styled-components"
-  ]
+  ],
+  setupFiles: ["<rootDir>/jestSetupFile.js"]
 }
--- src/__tests__/hooks/auth.spec.tsx
+++ src/__tests__/hooks/auth.spec.tsx
@@ -1,14 +1,40 @@
 import { renderHook, act } from '@testing-library/react-hooks';
 import { AuthProvider, useAuth } from '../../hooks/auth';
+import fetchMock from 'jest-fetch-mock';
+
+fetchMock.enableMocks();
+
+const userTest = {
+    id: 'any_id',
+    email: 'john.doe@email.com',
+    given_name: 'John Doe',
+    picture: 'any_photo.png'
+};
+
+jest.mock('expo-auth-session', () => {
+    return {
+        startAsync: () => ({
+            type: 'success',
+            params: {
+                access_token: 'any_token',
+            }
+        }),
+    }
+})
 
 describe('Auth Hook', () => {
     it('should be able to sign in with Google account', async () => {
+        fetchMock.mockResponseOnce(JSON.stringify(userTest));
+
         const { result } = renderHook(() => useAuth(), {
             wrapper: AuthProvider
         });
 
         await act(() => result.current.signInWithGoogle());
 
-        expect(result.current.user).toBeTruthy();
+        expect(result.current.user.id).toBe(userTest.id);
+        expect(result.current.user.name).toBe(userTest.given_name);
+        expect(result.current.user.email).toBe(userTest.email);
+        expect(result.current.user.photo).toBe(userTest.picture);
     });
 });
