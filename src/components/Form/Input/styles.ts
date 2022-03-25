79a55725bd33c51ba9aeb3962fef7ed70a16081f
Luis Fernando Serra <lfserrra@gmail.com>
2022-03-25 15:42:21 -0300

    Iniciando com testes

--- src/components/Form/Input/styles.ts
+++ src/components/Form/Input/styles.ts
@@ -1,7 +1,11 @@
+import styled, { css } from "styled-components/native";
 import { RFValue } from "react-native-responsive-fontsize";
-import styled from "styled-components/native";
 
-export const Container = styled.TextInput`
+interface Props {
+    active: boolean;
+};
+
+export const Container = styled.TextInput<Props>`
     width: 100%;
     padding: 16px 18px;
     font-size: ${RFValue(14)}px;
@@ -10,4 +14,9 @@ export const Container = styled.TextInput`
     background-color: ${({ theme }) => theme.colors.shape};
     border-radius: 5px;
     margin-bottom: 8px;
+
+    ${({ theme, active }) => active && css`
+        border-width: 3px;
+        border-color: ${theme.colors.attention};
+    `}
 `;
